e1c102d751cbe6b37edc714661da42ad8e22822b
aka <akatsukisatoshi@gmail.com>
2022-07-15 01:35:22 +0900

    複数の画像を送信できるところまで実装

---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const props = defineProps<{
+  imgFile: File;
+}>();
+const imgSrc = URL.createObjectURL(props.imgFile);
+</script>
+
+<template><img :src="imgSrc" /></template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ImagePreview from "@/components/ImagePreview.vue";
+import { ref } from "vue";
+
+const imgFiles = ref<File[]>([]);
+
+const onChange = (e: Event) => {
+  const inputEvent = e.target as HTMLInputElement;
+  const targetFiles = inputEvent.files as FileList;
+  const filesArray = Array.from(targetFiles);
+  imgFiles.value.push(...filesArray);
+};
+
+const submit = async () => {
+  const formData = new FormData();
+  imgFiles.value.forEach((file, index) => {
+    formData.append(`files[${index}]`, file);
+  });
+  const response = await fetch("https://httpbin.org/post", {
+    method: "POST",
+    body: formData,
+  });
+  const responseJson = await response.json();
+  console.log(responseJson);
+  imgFiles.value = [];
+};
+</script>
 
 <template>
-  <main>Hello</main>
+  <form @submit.prevent>
+    <label for="image">画像を選択してください</label>
+
+    <input
+      @change="onChange"
+      type="file"
+      id="image"
+      name="image"
+      accept="image/*"
+      multiple
+    />
+    <button type="submit" @click="submit">保存</button>
+  </form>
+
+  <div v-for="(imgFile, index) in imgFiles" :key="index">
+    <ImagePreview :imgFile="imgFile" />
+  </div>
 </template>
